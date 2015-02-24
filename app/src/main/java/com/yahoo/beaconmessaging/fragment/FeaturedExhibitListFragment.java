2a666f56af3878dbf212b3de7a82751a4bf39c3e
saianudeepm <sai.anudeep.m@gmail.com>
2015-02-24 14:50:00 -0800

    fixed the errors.. project should run now although not wired up the home activity to the stream yet

--- app/src/main/java/com/yahoo/beaconmessaging/fragment/FeaturedExhibitListFragment.java
+++ app/src/main/java/com/yahoo/beaconmessaging/fragment/FeaturedExhibitListFragment.java
@@ -1,4 +1,13 @@
 package com.yahoo.beaconmessaging.fragment;
 
 public class FeaturedExhibitListFragment extends ExhibitListFragment {
+    @Override
+    protected void populateStream() {
+
+    }
+
+    @Override
+    protected void loadMoreTimeline() {
+
+    }
 }
--- app/src/main/java/com/yahoo/beaconmessaging/fragment/NearbyExhibitListFragment.java
+++ app/src/main/java/com/yahoo/beaconmessaging/fragment/NearbyExhibitListFragment.java
@@ -2,4 +2,13 @@ package com.yahoo.beaconmessaging.fragment;
 
 
 public class NearbyExhibitListFragment extends ExhibitListFragment {
+    @Override
+    protected void populateStream() {
+
+    }
+
+    @Override
+    protected void loadMoreTimeline() {
+
+    }
 }
