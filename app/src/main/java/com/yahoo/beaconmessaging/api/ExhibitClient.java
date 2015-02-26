04d554d0319447372e6f4d2309fd068fa11defea
rneel <rneel@yahoo-inc.com>
2015-02-25 19:26:13 -0800

    added api to ExhibitClient

--- app/src/main/java/com/yahoo/beaconmessaging/api/ExhibitClient.java
+++ app/src/main/java/com/yahoo/beaconmessaging/api/ExhibitClient.java
@@ -3,6 +3,9 @@ package com.yahoo.beaconmessaging.api;
 import com.parse.FindCallback;
 import com.parse.ParseQuery;
 import com.yahoo.beaconmessaging.model.Exhibit;
+import com.yahoo.beaconmessaging.model.Post;
+
+import java.util.List;
 
 /**
  * Created by saianudeepm on 2/25/15.
@@ -14,5 +17,12 @@ public class ExhibitClient {
         ParseQuery<Exhibit> query = ParseQuery.getQuery(Exhibit.class);
         query.whereEqualTo("featured", true);
         query.findInBackground(findCallback);
+    }
+    
+    public static void  getCommentsForExhibit(String exhibitId, FindCallback<Post> postFindCallback)
+    {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.whereEqualTo("exhibitId", exhibitId);
+        query.findInBackground( postFindCallback);
     }
 }
