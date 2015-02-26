4455436b93068391bdecfe3d9296c41972468806
rneel <rneel@yahoo-inc.com>
2015-02-25 20:19:28 -0800

    updating post counts on exhibit and user

--- app/src/main/java/com/yahoo/beaconmessaging/api/ExhibitClient.java
+++ app/src/main/java/com/yahoo/beaconmessaging/api/ExhibitClient.java
@@ -1,7 +1,14 @@
 package com.yahoo.beaconmessaging.api;
 
+import android.util.Log;
+
 import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 import com.yahoo.beaconmessaging.model.Exhibit;
 import com.yahoo.beaconmessaging.model.Post;
 
@@ -24,5 +31,26 @@ public class ExhibitClient {
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
         query.whereEqualTo("exhibitId", exhibitId);
         query.findInBackground( postFindCallback);
+    }
+    
+    public static void addPost(Post post, SaveCallback saveCallback)
+    {
+        ParseUser user = ParseUser.getCurrentUser();
+        user.increment("postCount");
+        user.saveInBackground();
+        
+        ParseQuery<Exhibit> query = ParseQuery.getQuery(Exhibit.class);
+        query.getInBackground(post.getString("exhibitId"), new GetCallback<Exhibit>() {
+            @Override
+            public void done(Exhibit exhibit, ParseException e) {
+                if (exhibit != null) {
+                    Log.d("ExhibitClient", "Exhibit obtained has:" + exhibit.getInt("postCount") + ": posts");
+                    exhibit.increment("postCount");
+                    exhibit.saveInBackground();
+                }
+            }
+        });
+
+        post.saveInBackground(saveCallback);
     }
 }
